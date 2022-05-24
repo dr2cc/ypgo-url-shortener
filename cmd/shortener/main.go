7c4c558bb9def5f87ac7b083a651321052fb2878
belamov <business@belamov.com>
2022-05-24 08:49:12 +0500

    test

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
