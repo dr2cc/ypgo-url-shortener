0a2a43254f53671f00bdf6ac2fc2862f2f07420c
belamov <business@belamov.com>
2022-05-24 09:45:27 +0500

    added response statuses

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,8 +6,34 @@ import (
 )
 
 func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(ShortenerHandler))
+
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func ShortenerHandler(w http.ResponseWriter, r *http.Request) {
+	// проверяем, каким методом получили запрос
+	switch r.Method {
+	// если методом POST
+	case http.MethodGet:
+		getShortUrl(w, r)
+	case http.MethodPost:
+		saveShortUrl(w, r)
+	default:
+		http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func saveShortUrl(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getShortUrl(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusTemporaryRedirect)
+
 }
