87472859370682dfc13d4bf81720d457491e1bcc
belamov <business@belamov.com>
2022-07-03 17:41:53 +0500

    added docker environment; fixed migrations

--- internal/app/storage/pgsql_repository.go
+++ internal/app/storage/pgsql_repository.go
@@ -44,8 +44,8 @@ func runMigrations(conn *pgx.Conn) error {
 
 	t, err := conn.Exec(context.Background(), "create table if not exists urls("+
 		"created_by varchar(36) not null, "+
-		"original_url varchar() not null, "+
-		"id varchar(8) unique not null"+
+		"original_url varchar not null, "+
+		"id varchar(12) unique not null"+
 		");")
 	fmt.Println(t)
 	if err != nil {
