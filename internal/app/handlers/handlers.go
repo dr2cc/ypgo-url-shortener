a4674442cf0db2913c758868398429bd5e94c1cb
belamov <business@belamov.com>
2022-07-14 21:37:22 +0500

    debugging

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -347,6 +348,8 @@ func (h *Handler) DeleteUrls(w http.ResponseWriter, r *http.Request) {
 	}
 
 	userID := h.getUserID(r)
+
+	fmt.Printf("recieved ids: %v\n", ids)
 
 	go h.service.DeleteUrls(ids, userID)
 
