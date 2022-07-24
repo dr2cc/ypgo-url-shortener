a338778a1b9aaeeae6ae0f889c60231fecda751f
belamov <business@belamov.com>
2022-07-24 13:37:59 +0500

    added read heaer timeout

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/belamov/ypgo-url-shortener/internal/app/config"
 	"github.com/belamov/ypgo-url-shortener/internal/app/handlers"
@@ -18,8 +19,9 @@ func (s *Server) Run() {
 	r := handlers.NewRouter(s.service, s.config)
 
 	httpServer := &http.Server{
-		Addr:    s.config.ServerAddress,
-		Handler: r,
+		Addr:              s.config.ServerAddress,
+		Handler:           r,
+		ReadHeaderTimeout: 1 * time.Second,
 	}
 	log.Fatal(httpServer.ListenAndServe())
 }
