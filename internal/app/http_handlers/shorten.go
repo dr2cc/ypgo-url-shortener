040b4b46b1599b7f4e0e7e52f2109afc1133ed6f
Constantine <tmail99@gmail.com>
2026-01-13 22:52:25 +0300

    ❌Явное (и грубое!) нарушение слоев! Мало того, что слой хендлеров занимается "проверкой" (!), так он еще и напрямую обращается к хранилищу!!

--- internal/app/http_handlers/shorten.go
+++ internal/app/http_handlers/shorten.go
@@ -35,6 +35,11 @@ func (h *Handler) Shorten(w http.ResponseWriter, r *http.Request) {
 	userID := h.getUserID(r)
 	// отсюда вход в текстовый сократитель
 	shortURL, err := h.service.Shorten(r.Context(), string(url), userID)
+
+	// ❌ Явное (и грубое!) нарушение слоев!
+	// Мало того, что слой хендлеров занимается "проверкой" (!),
+	// так он еще и напрямую обращается к хранилищу!!
+	//
 	// iter13 - уникальный индекс и ошибка 409
 	var notUniqueErr *storage.NotUniqueURLError
 	// func errors.As(err error, target any) bool
